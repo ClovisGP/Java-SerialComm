df87335f1bfab26558a5dc52b5cc458c8d8394e7
Clovis GILLES GITKRAKEN <c.gillespinatel@we-deal.fr>
2024-04-24 11:47:41 +0200

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,26 +15,11 @@ public class Main {
                 return true;
             return msgParsed.type == LCBHelper.KeyWords.RES;
         };
-        //Thread.sleep(10000);
         serialCommunication.sendRequest(LCBReqGenerator.RequestList.READ_CRC, callback);
-        Thread.sleep(1000);
         //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, callback, 1, 2);
         Thread.sleep(1000);
         //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, callback, 1, 3);
-        Thread.sleep(1000);
-        //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, callback, 1, 4);
-        Thread.sleep(1000);
-        //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, callback, 1, 5);
-        Thread.sleep(1000);
-        //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, callback, 1, 6);
         //Thread.sleep(1000);
         serialCommunication.sendRequest(LCBReqGenerator.RequestList.READ_BATTERY, callback);
-//        serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, s -> System.out.println("Consumer 1: " + s), 1, 3);
-//
-//        serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, s -> System.out.println("Consumer 1: " + s), 1, 4);//Doesn't pass the arg
-//        serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, s -> System.out.println("Consumer 1: " + s), 1, 5);
-        //serialCommunication.sendRequest(LCBReqGenerator.RequestList.OPEN_DOOR, s -> System.out.println("Consumer 1: " + s), 1, 6);
-        // Do your work
-        //serialCommunication.closePort();
     }
 }
